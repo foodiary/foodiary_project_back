9554309bbf24b3f55ee287cd892d0e2dab7d29a4
meeejyu <alwn04016@naver.com>
2022-12-19 23:07:30 +0900

    Feat: project init

---
+++ foodiary/src/main/java/com/foodiary/FoodiaryApplication.java
@@ -0,0 +1,13 @@
+package com.foodiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiaryApplication.class, args);
+	}
+
+}
---
+++ foodiary/src/test/java/com/foodiary/FoodiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
