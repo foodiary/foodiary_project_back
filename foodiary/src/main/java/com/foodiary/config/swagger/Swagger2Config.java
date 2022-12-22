9dc1bd53a05628fdcb7927fe3d5509f098a37045
meeejyu <alwn04016@naver.com>
2022-12-22 14:14:35 +0900

    feat "swagger setting" 시큐리티컨피그 간단하게 구성, swagger 설정

---
+++ foodiary/src/main/java/com/foodiary/config/swagger/Swagger2Config.java
@@ -0,0 +1,28 @@
+package com.foodiary.config.swagger;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+// 스웨거 사용 기본 설정
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("foodiary API")
+                        .description("foodiary 프로젝트 API 명세서입니다.")
+                        .version("v0.0.1"));
+    }
+}
