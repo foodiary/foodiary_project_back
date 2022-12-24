65ad4b4838be868778248f7644f1b60feed85816
Mintaek_CHO <mintaek5555@gmail.com>
2022-12-25 08:43:19 +0900

    feat : mybatis 설정

---
+++ foodiary/src/main/java/com/foodiary/member/mapper/MemberMapper.java
@@ -0,0 +1,15 @@
+package com.foodiary.member.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.foodiary.member.dto.MemberPostDto;
+import com.foodiary.member.entity.Member;
+
+@Mapper
+public interface MemberMapper {
+
+    void saveMember(MemberPostDto memberPostDto);
+
+    Member findByEmail(String email);
+    
+}
