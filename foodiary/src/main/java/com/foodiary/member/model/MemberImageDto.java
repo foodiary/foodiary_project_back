24215ff355733fe26a0dbcdbd2629b0ecd0b5fd7
meeejyu <alwn04016@naver.com>
2022-12-30 23:35:52 +0900

    feat "이미지 dto 설명 추가"

--- foodiary/src/main/java/com/foodiary/member/model/MemberImageDto.java
+++ foodiary/src/main/java/com/foodiary/member/model/MemberImageDto.java
@@ -16,22 +16,22 @@ public class MemberImageDto {
     @ApiModelProperty(value="사용자 시퀀스", required = true)
     private int memberId;
     
-    @ApiModelProperty(value="이미지 원본 파일명", required = true)
+    @ApiModelProperty(value="이미지 원본 파일명, test.png면 test", required = true)
     private String originalName;
 
-    @ApiModelProperty(value="이미지 원본 파일명(확장자 포함)", required = true)
+    @ApiModelProperty(value="이미지 원본 파일명(확장자 포함) test.png", required = true)
     private String originalFullName;
 
-    @ApiModelProperty(value="저장한 파일명", required = true)
+    @ApiModelProperty(value="저장한 파일명 uuid+밀리초", required = true)
     private String saveName;
 
-    @ApiModelProperty(value="저장한 경로", required = true)
+    @ApiModelProperty(value="저장한 경로 s3 주소", required = true)
     private String path;
 
-    @ApiModelProperty(value="이미지 크기", required = true)
+    @ApiModelProperty(value="이미지 크기, byte를 기준으로 저장", required = true)
     private long size;
 
-    @ApiModelProperty(value="이미지 확장자", required = true)
+    @ApiModelProperty(value="이미지 확장자, png", required = true)
     private String ext;
 
 }
