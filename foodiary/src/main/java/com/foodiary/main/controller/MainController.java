20922ca066b1cb98f6514ee3cb58319f598dbcea
meeejyu <alwn04016@naver.com>
2023-01-02 23:21:07 +0900

    feat "aws 헬스 체크"

--- foodiary/src/main/java/com/foodiary/main/controller/MainController.java
+++ foodiary/src/main/java/com/foodiary/main/controller/MainController.java
@@ -1,6 +1,17 @@
 package com.foodiary.main.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
     
-    
+    // aws 헬스 체크
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+
+        return new ResponseEntity<>("ok", HttpStatus.OK) ;
+    }
 }
