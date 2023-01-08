1f008f227da476e2cd6a157bfb54bca6e9e0f7b4
meeejyu <alwn04016@naver.com>
2023-01-08 11:49:04 +0900

    feat : "공지사항 및 faq dto 추가"

---
+++ foodiary/src/main/java/com/foodiary/member/model/MemberNoticeDto.java
@@ -0,0 +1,28 @@
+package com.foodiary.member.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberNoticeDto {
+    
+    @ApiModelProperty(value="공지 시퀀스", required = true)
+    private int noticeId;
+
+    @ApiModelProperty(value="공지 제목", required = true)
+    private String noticeTitle;
+
+    @ApiModelProperty(value="공지 작성자", required = true)
+    private String noticeWriter;
+
+    @ApiModelProperty(value="공지 내용", required = true)
+    private String noticeContent;
+
+    @ApiModelProperty(value="공지 일자", required = true)
+    private String noticeCreate;
+
+}
