0ae46a398ae06c26c74454ae788a9a9325079bec
meeejyu <alwn04016@naver.com>
2023-01-09 19:29:53 +0900

    feat : "레시피 검색, 하루식단 검색 dto 추가"

---
+++ foodiary/src/main/java/com/foodiary/search/model/SearchDailyResponseDto.java
@@ -0,0 +1,34 @@
+package com.foodiary.search.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchDailyResponseDto {
+    
+    @ApiModelProperty(value="레시피 시퀀스", required = true)
+    private int dailyId;
+    
+    @ApiModelProperty(value="게시글 제목", required = true)
+    private String dailyTitle;
+
+    @ApiModelProperty(value="게시글 직상지", required = true)
+    private String dailyWriter;
+
+    @ApiModelProperty(value="게시글 이미지 경로1", required = true)
+    private String dailyPath1;
+
+    @ApiModelProperty(value="게시글 댓글 수", required = true)
+    private int dailyComment;
+
+    @ApiModelProperty(value="게시글 조회 수", required = true)
+    private int dailyView;
+
+    @ApiModelProperty(value="게시글 좋아요 수", required = true)
+    private int dailyLike;
+
+}
