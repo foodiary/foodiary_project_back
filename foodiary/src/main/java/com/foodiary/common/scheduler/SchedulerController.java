8210fa2c5caa163a6e44d1e3b778deafac5aeca5
Mintaek_CHO <mintaek5555@gmail.com>
2023-01-15 08:59:47 +0900

    refactor : 스케줄러 확인 결과 SUN으로 할 경우 토요일 자정에 로직 실행되서 일요일 자정에 실행되도록 MON으로 변경

--- foodiary/src/main/java/com/foodiary/common/scheduler/SchedulerController.java
+++ foodiary/src/main/java/com/foodiary/common/scheduler/SchedulerController.java
@@ -43,7 +43,7 @@ public class SchedulerController {
 
     }
 
-    @Scheduled(cron="0 0 * * * SUN")
+    @Scheduled(cron="0 0 * * * MON")
     public void menuScheduler() {
 
         List<MemberDto> memberList = memberMapper.findAll();
