325e96898c6d75caa8b427da1524f920c7d9318e
meeejyu <alwn04016@naver.com>
2023-01-20 17:42:09 +0900

    feat : "회원 닉네임 정책 수정"

--- foodiary/src/main/java/com/foodiary/member/model/MemberCheckNicknameRequestDto.java
+++ foodiary/src/main/java/com/foodiary/member/model/MemberCheckNicknameRequestDto.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 public class MemberCheckNicknameRequestDto {
     
     @NotBlank(message = "닉네임이 비어있습니다.")
-    @Pattern(regexp = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]{2,16}$", message = "닉네임은 2~16자리 한글, 영어 숫자만 가능합니다.")
+    @Pattern(regexp = "^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]{2,10}$", message = "닉네임은 2~10자리 한글, 영어 숫자만 가능합니다.")
     @ApiModelProperty(value="사용자 닉네임", required = true)
     private String nickName;
 
